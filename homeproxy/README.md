a6151c8fc03b9434971fe99021c571a5685f66c4
Malik Al <kidznightserver@gmail.com>
2023-12-27 13:51:51 +0700

    Add Support Homeproxy

---
+++ homeproxy/README.md
@@ -0,0 +1,96 @@
+# Homeproxy Tutorial
+
+Tutorial ini hanya untuk OpenWRT 23.05 atau terbaru dengan firewall4 sebagai default.
+
+Langkah-langkah WAJIB harus ada internet
+- Installation sing-box
+- Installation Homeproxy
+- Patch Homeproxy
+- Download GeoIP and GeoSite
+- Config Homeproxy
+
+Sebelum restart OpenWRT, Mohon untuk mematikan auto startup Tools inject lainnya seperti OpenClash dkk.
+
+# Installation sing-box
+
+Silahkan lakukan instalasi sing-box melalui opkg
+```sh
+opkg update;opkg install sing-box
+```
+Jika tidak bisa, bisa menggunakan alternative download manual, contoh saya menggunakan Raspberry Pi dengan arch arm64.
+```sh
+cd /tmp
+wget -O /tmp/sing-box-1.7.6-linux-arm64.tar.gz https://github.com/SagerNet/sing-box/releases/download/v1.7.6/sing-box-1.7.6-linux-arm64.tar.gz
+tar -xvf /tmp/sing-box-1.7.6-linux-arm64.tar.gz
+cp /tmp/sing-box-1.7.6-linux-arm64/sing-box /usr/bin/sing-box
+```
+
+# Installation Homeproxy
+
+Homeproxy untuk sekarang hanya support untuk sing-box sebelum versi 1.8.0 jadi patch dan config yang kami sediakan juga menyesuaikan.
+Download luci-app-homeproxy
+```sh
+wget -O /tmp/luci-app-homeproxy_release-2023122200_all.ipk https://github.com/douglarek/luci-app-homeproxy/releases/download/2023122200/luci-app-homeproxy_release-2023122200_all.ipk
+```
+Install
+```sh
+opkg install /tmp/luci-app-homeproxy_release-2023122200_all.ipk
+```
+
+**JANGAN BUKA HOMEPROXY DI LUCI TERLEBIH DAHULU**
+
+## Patch Homeproxy
+
+**JANGAN BUKA HOMEPROXY DI LUCI TERLEBIH DAHULU**
+
+Patch ini menggunakan custom geo dari [malikshi](https://t.me/synricha) selaku owner/maintener [IPTUNNELS](https://join.iptunnels.com/).
+Install Patch 1
+```sh
+wget -O /etc/homeproxy/scripts/generate_client.uc https://raw.githubusercontent.com/IPTUNNELS/IPTUNNELS/main/homeproxy/patch/generate_client.uc && chmod +x /etc/homeproxy/scripts/generate_client.uc
+```
+Install Patch 2
+```sh
+wget -O /etc/homeproxy/scripts/update_resources.sh https://raw.githubusercontent.com/IPTUNNELS/IPTUNNELS/main/homeproxy/patch/update_resources.sh && chmod +x /etc/homeproxy/scripts/update_resources.sh
+```
+
+## Download GeoIP and GeoSite
+
+**JANGAN BUKA HOMEPROXY DI LUCI TERLEBIH DAHULU**
+
+Download GEOIP
+```sh
+./update_resources.sh geoip
+```
+
+Download GEOSITE
+```sh
+./update_resources.sh geosite
+```
+
+## Config Homeproxy
+
+**JANGAN BUKA HOMEPROXY DI LUCI TERLEBIH DAHULU**
+
+**JANGAN EDIT CONFIG HOMEPROXY MELALUI TERMINAL JIKA TIDAK PAHAM, GUNAKAN LUCI!**
+
+Download config base yang IPTUNNELS sediakan untuk config homeproxy
+```sh
+wget -O /etc/config/homeproxy https://raw.githubusercontent.com/IPTUNNELS/IPTUNNELS/main/homeproxy/homeproxy-config
+```
+Apply Config
+```
+uci commit homeproxy
+```
+
+# Restart OpenWRT Devices
+
+Beberapa kali test mendapati error json ketika akses halaman Services Homeproxy, oleh karena itu sangat disarankan untuk restart devices.
+
+# Node Settings 
+
+Pengaturan Akun Node IPTUNNELS atau server lain pada Node Settings, Silahkan Edit masing-masing node Save, Jika Semua sudah diedit/hapus Node yang terpakai, Silahkan Save & Apply.
+
+Kalian bisa mengecek log melalui Services Status, Namun Saya Lebih rekomendasikan untuk mengecek melalui terminal
+```sh
+tail -f /var/run/homeproxy/sing-box-c.log
+```
---
+++ homeproxy/patch/update_resources.sh
@@ -0,0 +1,196 @@
+#!/bin/sh
+# SPDX-License-Identifier: GPL-2.0-only
+#
+# Copyright (C) 2022-2023 ImmortalWrt.org
+
+NAME="homeproxy"
+
+RESOURCES_DIR="/etc/$NAME/resources"
+mkdir -p "$RESOURCES_DIR"
+
+RUN_DIR="/var/run/$NAME"
+LOG_PATH="$RUN_DIR/$NAME.log"
+mkdir -p "$RUN_DIR"
+
+log() {
+	echo -e "$(date "+%Y-%m-%d %H:%M:%S") $*" >> "$LOG_PATH"
+}
+
+set_lock() {
+	local act="$1"
+	local type="$2"
+
+	local lock="$RUN_DIR/update_resources-$type.lock"
+	if [ "$act" = "set" ]; then
+		if [ -e "$lock" ]; then
+			log "[$(to_upper "$type")] A task is already running."
+			exit 2
+		else
+			touch "$lock"
+		fi
+	elif [ "$act" = "remove" ]; then
+		rm -f "$lock"
+	fi
+}
+
+to_upper() {
+	echo -e "$1" | tr "[a-z]" "[A-Z]"
+}
+
+check_clash_dashboard_update() {
+	local cdtype="$1"
+	local cdrepo="$2"
+	local wget="wget --timeout=30 -q"
+
+	set_lock "set" "$cdtype"
+
+	local cddata_ver="$($wget -O- "https://api.github.com/repos/$cdrepo/releases/latest" | jsonfilter -e "@.tag_name")"
+	if [ -z "$cddata_ver" ]; then
+		log "[$(to_upper "$cdtype")] Failed to get the latest version, please retry later."
+
+		set_lock "remove" "$cdtype"
+		return 1
+	fi
+
+	local local_cddata_ver="$(cat "$RESOURCES_DIR/$cdtype.ver" 2>"/dev/null" || echo "NOT FOUND")"
+	if [ "$local_cddata_ver" = "$cddata_ver" ]; then
+		log "[$(to_upper "$cdtype")] Current version: $cddata_ver."
+		log "[$(to_upper "$cdtype")] You're already at the latest version."
+
+		set_lock "remove" "$cdtype"
+		return 3
+	else
+		log "[$(to_upper "$cdtype")] Local version: $local_cddata_ver, latest version: $cddata_ver."
+	fi
+
+	$wget "https://github.com/$cdrepo/releases/download/$cddata_ver/compressed-dist.tgz" -O "$RESOURCES_DIR/compressed-dist.tgz" && \
+		rm -rf "$RESOURCES_DIR/ui" && mkdir -p "$RESOURCES_DIR/ui" && \
+		tar -zxf "$RESOURCES_DIR/compressed-dist.tgz" -C "$RESOURCES_DIR/ui" && \
+		rm -rf "$RESOURCES_DIR/compressed-dist.tgz"
+
+	echo -e "$cddata_ver" > "$RESOURCES_DIR/$cdtype.ver"
+	log "[$(to_upper "$cdtype")] Successfully updated."
+
+	set_lock "remove" "$cdtype"
+	return 0
+}
+
+check_geodata_update() {
+	local geotype="$1"
+	local georepo="$2"
+	local wget="wget --timeout=120 -q"
+
+	set_lock "set" "$geotype"
+
+	local geodata_ver="$($wget -O- "https://api.github.com/repos/$georepo/releases/latest" | jsonfilter -e "@.tag_name")"
+	if [ -z "$geodata_ver" ]; then
+		log "[$(to_upper "$geotype")] Failed to get the latest version, please retry later."
+
+		set_lock "remove" "$geotype"
+		return 1
+	fi
+
+	local local_geodata_ver="$(cat "$RESOURCES_DIR/$geotype.ver" 2>"/dev/null" || echo "NOT FOUND")"
+	if [ "$local_geodata_ver" = "$geodata_ver" ]; then
+		log "[$(to_upper "$geotype")] Current version: $geodata_ver."
+		log "[$(to_upper "$geotype")] You're already at the latest version."
+
+		set_lock "remove" "$geotype"
+		return 3
+	else
+		log "[$(to_upper "$geotype")] Local version: $local_geodata_ver, latest version: $geodata_ver."
+	fi
+
+	local geodata_hash
+	$wget "https://github.com/$georepo/releases/download/$geodata_ver/$geotype.db" -O "$RUN_DIR/$geotype.db"
+	geodata_hash="$($wget -O- "https://github.com/$georepo/releases/download/$geodata_ver/$geotype.db.sha256sum" | awk '{print $1}')"
+	if ! echo -e "$geodata_hash $RUN_DIR/$geotype.db" | sha256sum -s -c -; then
+		rm -f "$RUN_DIR/$geotype.db"
+		log "[$(to_upper "$geotype")] Update failed."
+
+		set_lock "remove" "$geotype"
+		return 1
+	fi
+
+	mv -f "$RUN_DIR/$geotype.db" "$RESOURCES_DIR/$geotype.db"
+	echo -e "$geodata_ver" > "$RESOURCES_DIR/$geotype.ver"
+	log "[$(to_upper "$geotype")] Successfully updated."
+
+	set_lock "remove" "$geotype"
+	return 0
+}
+
+check_list_update() {
+	local listtype="$1"
+	local listrepo="$2"
+	local listref="$3"
+	local listname="$4"
+	local wget="wget --timeout=10 -q"
+
+	set_lock "set" "$listtype"
+
+	local list_info="$($wget -O- "https://api.github.com/repos/$listrepo/commits?sha=$listref&path=$listname")"
+	local list_sha="$(echo -e "$list_info" | jsonfilter -e "@[0].sha")"
+	local list_ver="$(echo -e "$list_info" | jsonfilter -e "@[0].commit.message" | grep -Eo "[0-9-]+" | tr -d '-')"
+	if [ -z "$list_sha" ] || [ -z "$list_ver" ]; then
+		log "[$(to_upper "$listtype")] Failed to get the latest version, please retry later."
+
+		set_lock "remove" "$listtype"
+		return 1
+	fi
+
+	local local_list_ver="$(cat "$RESOURCES_DIR/$listtype.ver" 2>"/dev/null" || echo "NOT FOUND")"
+	if [ "$local_list_ver" = "$list_ver" ]; then
+		log "[$(to_upper "$listtype")] Current version: $list_ver."
+		log "[$(to_upper "$listtype")] You're already at the latest version."
+
+		set_lock "remove" "$listtype"
+		return 3
+	else
+		log "[$(to_upper "$listtype")] Local version: $local_list_ver, latest version: $list_ver."
+	fi
+
+	$wget "https://fastly.jsdelivr.net/gh/$listrepo@$list_sha/$listname" -O "$RUN_DIR/$listname"
+	if [ ! -s "$RUN_DIR/$listname" ]; then
+		rm -f "$RUN_DIR/$listname"
+		log "[$(to_upper "$listtype")] Update failed."
+
+		set_lock "remove" "$listtype"
+		return 1
+	fi
+
+	mv -f "$RUN_DIR/$listname" "$RESOURCES_DIR/$listtype.${listname##*.}"
+	echo -e "$list_ver" > "$RESOURCES_DIR/$listtype.ver"
+	log "[$(to_upper "$listtype")] Successfully updated."
+
+	set_lock "remove" "$listtype"
+	return 0
+}
+
+case "$1" in
+"clash_dashboard")
+	check_clash_dashboard_update "$1" "MetaCubeX/metacubexd"
+	;;
+"geoip")
+	check_geodata_update "$1" "malikshi/sing-box-geo"
+	;;
+"geosite")
+	check_geodata_update "$1" "malikshi/sing-box-geo"
+	;;
+"china_ip4")
+	check_list_update "$1" "douglarek/IPCIDR-CHINA" "master" "ipv4.txt"
+	;;
+"china_ip6")
+	check_list_update "$1" "douglarek/IPCIDR-CHINA" "master" "ipv6.txt"
+	;;
+"gfw_list")
+	check_list_update "$1" "Loyalsoldier/v2ray-rules-dat" "release" "gfw.txt"
+	;;
+"china_list")
+	check_list_update "$1" "Loyalsoldier/v2ray-rules-dat" "release" "direct-list.txt"
+	;;
+*)
+	echo -e "Usage: $0 <clash_dashboard/ geoip / geosite / china_ip4 / china_ip6 / gfw_list / china_list>"
+	exit 1
+	;;
+esac
